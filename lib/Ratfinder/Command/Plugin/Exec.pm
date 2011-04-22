b6dafe128bef320c48979c3d79c252dbf1ce9d05
kidd <raimonster@gmail.com>
2011-04-22 03:22:06 +0200

    fixed typo in Exec

--- lib/Ratfinder/Command/Plugin/Exec.pm
+++ lib/Ratfinder/Command/Plugin/Exec.pm
@@ -1,9 +1,8 @@
 package Ratfinder::Command::Plugin::Exec;
 use Moose::Role;
 use feature ':5.10';
-use Shell;
 
-sub keywords { my @comands=`dmenu_path`; chomp @commands; @commands,'exec' } ;
+sub keywords { my @commands=`dmenu_path`; chomp @commands; @commands,'exec' } ;
 
 sub exec {
   my $self = shift;
@@ -13,9 +12,9 @@ sub exec {
   $self->gui->echo(split(/\s+/, $cmd));
   system $cmd
 }
-  sub AUTOLOAD {
-    Shell->new
-  }
+  # sub AUTOLOAD {
+  #   Shell->new
+  # }
 
 #__PACKAGE__->meta->make_immutable;
 1; # End of Ratfinder::Plugin::Dictionary;
